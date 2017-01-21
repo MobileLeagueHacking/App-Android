76e6e7b854fccb2c5fb9ce3e7decdd1dd93d0ee0
AdamMc331 <amcneilly331@gmail.com>
2017-01-21 11:31:39 -0500

    Added start and end date now that they're a part of the API.

--- app/src/main/java/com/adammcneilly/mobileleaguehacking/models/Hackathon.kt
+++ app/src/main/java/com/adammcneilly/mobileleaguehacking/models/Hackathon.kt
@@ -1,11 +1,19 @@
 package com.adammcneilly.mobileleaguehacking.models
 
+import java.text.SimpleDateFormat
+import java.util.*
+
 /**
  * Represents a hackathon event.
  *
  * Created by adam.mcneilly on 1/21/17.
  */
 open class Hackathon {
+    /**
+     * Formatter to convert a string into a date.
+     */
+    val dateFormatter = SimpleDateFormat("yyyy-mm-dd", Locale.getDefault())
+
     /**
      * The URL to the main hackathon image.
      */
@@ -37,4 +45,28 @@ open class Hackathon {
     var location = ""
         // Since the API returns city,State without a space, let's add one ourselves.
         get() = field.replace(",", ", ")
+
+    /**
+     * The first date of the event.
+     */
+    var startDate = ""
+
+    /**
+     * The laste date of the event.
+     */
+    var endDate = ""
+
+    /**
+     * Returns a Date object for the startDate string.
+     */
+    fun getStartDate(): Date {
+        return dateFormatter.parse(startDate)
+    }
+
+    /**
+     * Returns a Date object for the endDate string.
+     */
+    fun getEndDate(): Date {
+        return dateFormatter.parse(endDate)
+    }
 }
