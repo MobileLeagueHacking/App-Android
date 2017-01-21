5e6f5d8226e1a5c14f693a30a6c7472c344049ca
AdamMc331 <amcneilly331@gmail.com>
2017-01-21 12:06:06 -0500

    Added swipe refresh layout.

--- app/src/main/java/com/adammcneilly/mobileleaguehacking/activities/MainActivity.kt
+++ app/src/main/java/com/adammcneilly/mobileleaguehacking/activities/MainActivity.kt
@@ -32,25 +32,39 @@ class MainActivity : AppCompatActivity() {
         hackathon_list.adapter = adapter
         hackathon_list.layoutManager = layoutManager
 
+        refresh_layout.setOnRefreshListener { getHackathons() }
+
+        getHackathons()
+    }
+
+    /**
+     * Retrieves and displays all hackathons.
+     */
+    private fun getHackathons() {
+        // If this was called via the refresh layout, don't bother to show progress bar too
+        if (!refresh_layout.isRefreshing) {
+            progress_bar.visibility = View.VISIBLE
+        }
+
         // Make call
         val api = MLHManager()
-        progress_bar.visibility = View.VISIBLE
         api.getHackathons()
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribeOn(Schedulers.newThread())
                 .subscribe(object: Subscriber<List<Hackathon>>() {
                     override fun onError(e: Throwable?) {
                         Timber.e(e)
+                        progress_bar.visibility = View.GONE
+                        refresh_layout.isRefreshing = false
                     }
 
                     override fun onNext(t: List<Hackathon>?) {
-                        if (t != null) {
-                            adapter.swapItems(t)
-                        }
+                        if (t != null) adapter.swapItems(t)
                     }
 
                     override fun onCompleted() {
                         progress_bar.visibility = View.GONE
+                        refresh_layout.isRefreshing = false
                     }
                 })
     }
