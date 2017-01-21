f7075b6a713b190a7b48d40024b113b392ff5567
AdamMc331 <amcneilly331@gmail.com>
2017-01-21 01:02:39 -0500

    Created hackathon view holder.

--- app/src/main/java/com/adammcneilly/mobileleaguehacking/MainActivity.kt
+++ app/src/main/java/com/adammcneilly/mobileleaguehacking/MainActivity.kt
@@ -16,12 +16,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val toolbar = findViewById(R.id.toolbar) as Toolbar
         setSupportActionBar(toolbar)
-
-        val fab = findViewById(R.id.fab) as FloatingActionButton
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
---
+++ app/src/main/java/com/adammcneilly/mobileleaguehacking/models/Hackathon.kt
@@ -0,0 +1,14 @@
+package com.adammcneilly.mobileleaguehacking.models
+
+/**
+ * Represents a hackathon event.
+ *
+ * Created by adam.mcneilly on 1/21/17.
+ */
+open class Hackathon {
+    var mainImageUrl = ""
+    var secondaryImageUrl = ""
+    var eventName = ""
+    var date = ""
+    var location = ""
+}
